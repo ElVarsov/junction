330a4f2dac946abfab315de9ab837f979dfddbaf
Aleksei Filonov <alexfiloonov@gmail.com>
2024-11-09 14:46:09 +0200

    Delete rn-client

--- rn-client/App.js
+++
@@ -1,7 +0,0 @@
-import { View } from "react-native";
-import { verifyInstallation } from "nativewind";
-
-export default function App() {
-  verifyInstallation();
-  return <View className="pt-20 px-6"></View>;
-}
